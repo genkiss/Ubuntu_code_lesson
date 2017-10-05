c7f9a38e94879346d2e76fdf8835a303827a71f8
genkiss <gengen.code@gmail.com>
2017-10-05 21:34:57 +0900

    commit

---
+++ C_lesson/List2/List2-4.c
@@ -0,0 +1,19 @@
+/*
+    読み込んだ整数値の符号を反転した値を表示
+*/
+
+#include (stdio.h)
+
+int main(void)
+{
+
+    int num;
+
+    printf("整数を入力してくだい:");
+    scanf("%d", &num);
+
+    printf("符号を反転した値は%dです。\n", -num);
+
+    return (0);
+
+}
