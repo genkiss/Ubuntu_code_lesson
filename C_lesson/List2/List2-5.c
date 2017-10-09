e4c22d33ed7db924824f94eb65d0217e91c236e3
genkiss <gengen.code@gmail.com>
2017-10-09 19:45:19 +0900

    List2-5

---
+++ C_lesson/List2/List2-5.c
@@ -0,0 +1,19 @@
+/*
+    ２つの整数を読み込んで平均値を表示
+*/
+
+#include<stdio.h>
+
+int main(void)
+{
+    int na, nb;
+
+    puts("２つの整数を入力してください。");
+    printf("整数A:"), scanf("%d", &na);
+    printf("整数B:"), scanf("%d", &nb);
+
+    printf("それらの平均は%dです。\n", (na + nb) / 2);
+
+    return (0);
+
+}
