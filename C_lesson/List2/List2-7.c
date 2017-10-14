42a93ca4507c2ad1fe0c4008ccf252f7e875331a
genkiss <gengen.code@gmail.com>
2017-10-14 19:29:05 +0900

    double 浮動小数点の計算

---
+++ C_lesson/List2/List2-7.c
@@ -0,0 +1,26 @@
+/*
+ *  2つの実数値を読み込んで和・差・積・商を実数で表示
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+  
+  double vx, vy;    /*浮動小数点数*/
+  
+  puts("２つの数を入力してください。");
+  printf("実数vx : "); scanf("%lf", &vx);
+  printf("実数vy : "); scanf("%lf", &vy);
+
+
+  printf("vx + vy = %f\n", vx + vy);
+  printf("vx - vy = %f\n", vx - vy);
+  printf("vx * vy = %f\n", vx * vy);
+  printf("vx / vy = %f\n", vx / vy);
+
+  return (0);
+
+
+}
+
