61865163055c83afbffa162a9dcfb79135f11a9b
genkiss <gengen.code@gmail.com>
2017-10-20 01:52:56 +0900

    夜の自習

---
+++ Teach Yourself_C/ch01/ch01-18.c
@@ -0,0 +1,23 @@
+/*
+ *  sumの使い方を示す簡単なプログラム
+ */
+
+#include <stdio.h>
+
+void sum(int x, int y);
+
+int main(void)
+{
+	sum(1, 20);
+	sum(9, 6);
+	sum(81,9);
+	
+	return 0;
+}
+
+void sum(int x, int y)
+{
+	printf("%d\n", x + y);
+}
+
+
